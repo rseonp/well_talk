26767a0329f948a93bfda33097cfc2445f44c447
Victor Li Wang <victorwang9@gmail.com>
2023-08-11 14:44:35 -0500

    loosened strictness of running project, added gridview when clicking attachement button, changed to icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'dart:convert';
+import 'dart:io';
 import 'dart:math';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
+import 'package:open_filex/open_filex.dart';
+import 'package:path_provider/path_provider.dart';
 
 // For the testing purposes, you should probably use https://pub.dev/packages/uuid.
 String randomString() {
@@ -41,8 +46,10 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) => Scaffold(
         body: Chat(
           messages: _messages,
+          onAttachmentPressed: _handleAttachmentPressed,
+          onMessageTap: _handleMessageTap,
+          onPreviewDataFetched: _handlePreviewDataFetched,
           onSendPressed: _handleSendPressed,
-          onAttachmentPressed: _handleImageSelection,
           user: _user,
         ),
       );
@@ -53,15 +60,86 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _handleSendPressed(types.PartialText message) {
-    final textMessage = types.TextMessage(
-      author: _user,
-      createdAt: DateTime.now().millisecondsSinceEpoch,
-      id: randomString(),
-      text: message.text,
+  void _handleAttachmentPressed() {
+    showModalBottomSheet<void>(
+        context: context,
+        builder: (BuildContext context) {
+          double width = MediaQuery.of(context).size.width;
+          double height = MediaQuery.of(context).size.height;
+          var padding = MediaQuery.of(context).padding;
+          double newheight = height - padding.top - padding.bottom;
+
+          return SafeArea(
+            child: SizedBox(
+              height: newheight / 3,
+              child: GridView.count(
+                scrollDirection: Axis.horizontal,
+                crossAxisCount: 2,
+                childAspectRatio: 0.5,
+                // Generate 6 widgets that display their index in the List.
+                children: List.generate(6, (index) {
+                  return Center(child: Icon(Icons.favorite)
+                      // child: Text(
+                      //   'Item $index',
+                      //   style: Theme.of(context).textTheme.headlineSmall,
+                      // ),
+                      );
+                }),
+              ),
+              // Column(
+              //   crossAxisAlignment: CrossAxisAlignment.stretch,
+              //   children: <Widget>[
+              //     TextButton(
+              //       onPressed: () {
+              //         Navigator.pop(context);
+              //         _handleImageSelection();
+              //       },
+              //       child: const Align(
+              //         alignment: AlignmentDirectional.centerStart,
+              //         child: Text('Photo'),
+              //       ),
+              //     ),
+              //     TextButton(
+              //       onPressed: () {
+              //         Navigator.pop(context);
+              //         _handleFileSelection();
+              //       },
+              //       child: const Align(
+              //         alignment: AlignmentDirectional.centerStart,
+              //         child: Text('File'),
+              //       ),
+              //     ),
+              //     TextButton(
+              //       onPressed: () => Navigator.pop(context),
+              //       child: const Align(
+              //         alignment: AlignmentDirectional.centerStart,
+              //         child: Text('Cancel'),
+              //       ),
+              //     ),
+              //   ],
+              // ),
+            ),
+          );
+        });
+  }
+
+  void _handleFileSelection() async {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.any,
     );
 
-    _addMessage(textMessage);
+    if (result != null && result.files.single.path != null) {
+      final message = types.FileMessage(
+        author: _user,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
+        id: randomString(),
+        name: result.files.single.name,
+        size: result.files.single.size,
+        uri: result.files.single.path!,
+      );
+
+      _addMessage(message);
+    }
   }
 
   void _handleImageSelection() async {
@@ -88,5 +166,75 @@ class _MyHomePageState extends State<MyHomePage> {
 
       _addMessage(message);
     }
+  }
+
+  void _handleMessageTap(BuildContext _, types.Message message) async {
+    if (message is types.FileMessage) {
+      var localPath = message.uri;
+
+      if (message.uri.startsWith('http')) {
+        try {
+          final index =
+              _messages.indexWhere((element) => element.id == message.id);
+          final updatedMessage =
+              (_messages[index] as types.FileMessage).copyWith(
+            isLoading: true,
+          );
+
+          setState(() {
+            _messages[index] = updatedMessage;
+          });
+
+          final client = http.Client();
+          final request = await client.get(Uri.parse(message.uri));
+          final bytes = request.bodyBytes;
+          final documentsDir = (await getApplicationDocumentsDirectory()).path;
+          localPath = '$documentsDir/${message.name}';
+
+          if (!File(localPath).existsSync()) {
+            final file = File(localPath);
+            await file.writeAsBytes(bytes);
+          }
+        } finally {
+          final index =
+              _messages.indexWhere((element) => element.id == message.id);
+          final updatedMessage =
+              (_messages[index] as types.FileMessage).copyWith(
+            isLoading: null,
+          );
+
+          setState(() {
+            _messages[index] = updatedMessage;
+          });
+        }
+      }
+
+      await OpenFilex.open(localPath);
+    }
+  }
+
+  void _handlePreviewDataFetched(
+    types.TextMessage message,
+    types.PreviewData previewData,
+  ) {
+    final index = _messages.indexWhere((element) => element.id == message.id);
+    final updatedMessage = (_messages[index] as types.TextMessage).copyWith(
+      previewData: previewData,
+    );
+
+    setState(() {
+      _messages[index] = updatedMessage;
+    });
+  }
+
+  void _handleSendPressed(types.PartialText message) {
+    final textMessage = types.TextMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: randomString(),
+      text: message.text,
+    );
+
+    _addMessage(textMessage);
   }
 }
